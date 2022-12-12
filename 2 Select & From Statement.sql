fc58a014d8a649b610b011f7b861cd62995f2f02
Firetiles <120344465+Firetiles@users.noreply.github.com>
2022-12-11 21:49:14 -0600

    Update 2 Select & From Statement.sql

--- 2 Select & From Statement.sql
+++ 2 Select & From Statement.sql
@@ -1,6 +1,21 @@
 /*
 select statement
-*, Top, Discount, Count, As Max, Min, Avg
+*, Top, Discount, Count, As, Max, Min, Avg
 */
-select *
+select top 5 *
+from [SQL Tutorial].dbo.EmployeeDemographics
+
+select distinct gender
+from [SQL Tutorial].dbo.EmployeeDemographics
+
+select distinct age
+from [SQL Tutorial].dbo.EmployeeDemographics
+
+select count(lastname) as 'Last Name Count'
+from [SQL Tutorial].dbo.EmployeeDemographics
+
+select min(salary) as MinSalary
+from [SQL Tutorial].dbo.EmployeeSalary
+
+select avg(salary) as MinSalary
 from [SQL Tutorial].dbo.EmployeeSalary
