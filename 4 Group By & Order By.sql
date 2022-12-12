c727cc060669f362ccd0f5f27b8dcdf36f123cf4
Firetiles <120344465+Firetiles@users.noreply.github.com>
2022-12-11 22:07:24 -0600

    Update 4 Group By & Order By.sql

--- 4 Group By & Order By.sql
+++ 4 Group By & Order By.sql
@@ -2,16 +2,31 @@
 Group By, Order By
 */
 
---select Gender, count(gender) as '>31'
---from EmployeeDemographics
---where age > 31
---group by Gender
---order by '>31' desc
+select distinct Gender
+from EmployeeDemographics
 
---select *
---from EmployeeDemographics
---order by Age desc, Gender desc
+select Gender
+from EmployeeDemographics
+group by gender
 
---select *
---from EmployeeDemographics
---order by 4 desc, 5 desc
+select Gender, count(Gender) as '# of'
+from EmployeeDemographics
+group by gender
+
+select Gender, Age, Count(Gender)
+from EmployeeDemographics
+group by gender, Age
+
+select Gender, count(gender) as '>31'
+from EmployeeDemographics
+where age > 31
+group by Gender
+order by '>31' desc
+
+select *
+from EmployeeDemographics
+order by Age desc, Gender desc
+
+select *
+from EmployeeDemographics
+order by 4 desc, 5 desc
