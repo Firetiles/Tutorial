825aebbd3b26ae09ebfd0fbee164ac8834f5bfb3
Firetiles <120344465+Firetiles@users.noreply.github.com>
2022-12-11 21:58:29 -0600

    Update 3 Where.sql

--- 3 Where.sql
+++ 3 Where.sql
@@ -3,15 +3,34 @@ Where Statement
 =, <>, <, >, And, Or, Like, Null, Not Null, In
 */
 
---select *
---from EmployeeDemographics
---where LastName like 'S%t%'
+select *
+from EmployeeDemographics
+where Age <>32 
 
+select *
+from EmployeeDemographics
+where Age <=32 and Gender = 'Male'
 
---select *
---from EmployeeDemographics
---where FirstName ='Jim' or FirstName ='Michael'
+select *
+from EmployeeDemographics
+where Age <=32 or Gender = 'Male'
 
---select *
---from EmployeeDemographics
---where FirstName in ('Jim','Michael')
+select *
+from EmployeeDemographics
+where FirstName is Null
+
+select *
+from EmployeeDemographics
+where FirstName is Not Null
+
+select *
+from EmployeeDemographics
+where LastName like 'S%t%'
+
+select *
+from EmployeeDemographics
+where FirstName ='Jim' or FirstName ='Michael'
+
+select *
+from EmployeeDemographics
+where FirstName in ('Jim','Michael')
